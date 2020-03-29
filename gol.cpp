0cce728752e356518ba42a0422803c176c2466d8
Anders Rassner <anders.rassner@gmail.com>
2020-03-29 12:27:34 +0200

    Added reading a starting state from file. Added two state files.

--- gol.cpp
+++ gol.cpp
@@ -1,6 +1,7 @@
 #include "GameBoard.hpp"
 #include <algorithm>
 #include <chrono>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
@@ -13,32 +14,45 @@ using std::cout, std::cerr, std::endl;
 
 int main(int argc, char * argv[]) {
   using namespace std::chrono_literals;
-  int expectedArguments{1};
   cout << "argc: " << argc << endl;
   for(int i{0}; i < argc ; ++i) {
     cout << "argument " << i+1 << ": " << argv[i] << endl;
   }
-  
-  if(!(argc == expectedArguments)) {
-    std::cerr << "please pass " << expectedArguments
-	      << "when running me" << std::endl;
-  }
 
-  // create default gameboard
-  GameBoard board{};
-  board.print();
-  board.next_board_state();
-  board.print();
-
-  GameBoard board120{30, 40};
-  board120.print();
-  auto start = std::chrono::system_clock::now();
-  while(board120.getNoCells() > 0
-	&& std::chrono::system_clock::now() < (start+10s)) {
-    board120.next_board_state();
+  if(argc == 1) { // create default gameboard
+    GameBoard board120{30, 40};
     board120.print();
-    std::this_thread::sleep_for(25ms);
+    auto start = std::chrono::system_clock::now();
+    while(board120.getNoCells() > 0
+	  && std::chrono::system_clock::now() < (start+10s)) {
+      board120.next_board_state();
+      board120.print();
+      std::this_thread::sleep_for(100ms);
+    }
   }
-  
+  else if(argc == 2) { // create board from file
+    std::ifstream ifs(argv[1]);
+    int rows {0};
+    int columns {0};
+    ifs >> rows;
+    ifs >> columns;
+    GameBoard board{columns, rows};
+    std::vector<int> stateFromFile{};
+    int x{0}; // TODO: replace with algorithm
+    while(ifs >> x) {
+      cout << "read: " << x << endl;
+      stateFromFile.push_back(x);
+    }
+    auto start = std::chrono::system_clock::now();
+    board.set_state(stateFromFile);
+    while(std::chrono::system_clock::now() < (start+10s)) {
+      board.next_board_state();
+      board.print();
+      std::this_thread::sleep_for(100ms);
+    }
+  }
+
+
+
   return 0;
 }
